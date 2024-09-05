c585553c3dec432e6c5e34899fd79f5c9540141a
Juanj024 <Juanjok247@gmail.com>
2024-09-04 20:10:13 -0400

    Done :)

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ hoursUsed=int(input("Enter an integer greater than zero representing the kilowat
 #If conditional to difference between the ones that are > or < 1000
 if(hoursUsed < 1000):
     hoursUsed = (hoursUsed * firstPriceDollars)
-    print ("Amount owed is","$","%.2f" % hoursUsed)
 
 elif(hoursUsed > 1000):
     limitHours = 1000 * firstPriceDollars
     extraHours= (hoursUsed - 1000) * secondPriceDollars
     hoursUsed = limitHours + extraHours
-    print("Amount owed is", "$", "%.2f" % hoursUsed)
+
+print("Amount owed is", "$", "%.2f" % hoursUsed)
